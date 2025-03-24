2aee8bd6be9df90decce403605d7142ae9a3d92a
Viet Nguyen <nqviet157@gmail.com>
2025-03-24 22:19:41 +0700

    change to webcomponent

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+class CartElement extends HTMLElement {
+  connectedCallback() {
+    createRoot(this.attachShadow({ mode: "open" })).render(
+      <StrictMode>
+        <App />
+      </StrictMode>
+    );
+  }
+}
+
+customElements.define("mfe-cart", CartElement);
